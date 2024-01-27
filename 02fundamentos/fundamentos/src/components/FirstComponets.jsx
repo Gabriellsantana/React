7666c0b958f20e58fba3ee7c9e0dcdfe79b798a3
Gabriel <gabrieldevspace@gmail.com>
2024-01-27 14:29:32 -0300

    criando componets

---
+++ 02fundamentos/fundamentos/src/components/FirstComponets.jsx
@@ -0,0 +1,11 @@
+const FirstComponenr = () =>{ //criando a função que vai retorna nossa aplicação o web
+    return(
+        <div>
+            <h1>Meu primeiro componente</h1>
+        </div>
+    )
+}
+
+export default FirstComponenr; //inportando para poder utlizar em qualquer modulo
+
+// exemplo se for criador um buttom aqui eu posso inporta esse buttom em varias locais do meu site semprecisar rescrever ele por completo apanesa chamando a função 
